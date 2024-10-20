d030035090ae12c262c5257664e886153d9a0d23
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-10-19 22:45:27 -0400

    Implemented navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,77 @@
-import React from "react";
-import { Link, useLocation } from 'react-router-dom';
-import '../styles/navbar.css';
-import logo from '../imgs/cuny-logo.jpg';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/navbar.css";
+import logo from "../imgs/cuny-logo.jpg";
 
-const Navbar = () =>{
-    const location = useLocation();
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const location = useLocation();
 
-    return(
-        <nav className="navbar">
-            <div className="logo">
-                    <img src={logo} alt="CUNY logo" />
-            </div>
-            {/* 
-            <div className="logo">
-                <Link to="/">
-                    <img src={logo} alt="CUNY logo" />
-                </Link>
-            </div>
-            <ul className="nav-links">
-                <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/campus">Campus</Link></li>
-                <li><Link to="/events">Events</Link></li>
-                <li><Link to="/resources">Resources</Link></li>
-            </ul>
-            */}
-        </nav>
-    );
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="CUNY logo" className="logo-img" />
+          </Link>
+        </div>
+        <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+          ☰
+        </button>
+        <ul className={`nav-links ${isMobileMenuOpen ? "open" : ""}`}>
+          <li>
+            <Link
+              to="/"
+              className={location.pathname === "/" ? "active" : ""}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={location.pathname === "/about" ? "active" : ""}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/campus"
+              className={location.pathname === "/campus" ? "active" : ""}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Campus
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/events"
+              className={location.pathname === "/events" ? "active" : ""}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/resources"
+              className={location.pathname === "/resources" ? "active" : ""}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Resources
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
